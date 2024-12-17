f23087185df5e7d0ace8482b79ea7bf0a7290e96
pamis-costa <pamelapereira321@gmail.com>
2024-12-17 15:28:20 -0300

    feat: add de exercicio 2 de javascript I

---
+++ javascript_I/exercicio2.js
@@ -0,0 +1,8 @@
+function operacoesBasicas(num1,num2){
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+operacoesBasicas(4, 5);
